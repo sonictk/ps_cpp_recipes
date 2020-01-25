40c4b7ef99c67435c2522c9d9d645267d5486e00
Siew Yi Liang <admin@sonictk.com>
2020-01-25 01:54:25 -0800

    more work on tutorial and using local version of stylesheet now

--- recipes/1_hello_world/src/tutorial_automation_main.cpp
+++ recipes/1_hello_world/src/tutorial_automation_main.cpp
@@ -1,4 +1,23 @@
+#include <Windows.h>
+#include <string.h>
+
+#define WIN32_MAX_CLASS_NAME_LENGTH 256
+
 SPBasicSuite *sSPBasic = NULL;
+HWND globalPSMainWindowHwnd = NULL;
+
+
+BOOL CALLBACK getPSMainWindowCB(HWND hwnd, LPARAM lParam)
+{
+	char windowClassName[WIN32_MAX_CLASS_NAME_LENGTH];
+	GetClassNameA(hwnd, (LPSTR)windowClassName, WIN32_MAX_CLASS_NAME_LENGTH);
+
+	if (strncmp(windowClassName, "Photoshop", WIN32_MAX_CLASS_NAME_LENGTH) == 0) {
+		globalPSMainWindowHwnd = hwnd;
+	}
+
+	return TRUE;
+}
 
 
 SPErr UninitializePlugin()
@@ -35,6 +54,9 @@ DLLExport SPAPI SPErr AutoPluginMain(const char* caller,
 	if (sSPBasic->IsEqual(caller, kPSPhotoshopCaller)) {
 		if (sSPBasic->IsEqual(selector, kPSDoIt)) {
 			PSActionsPlugInMessage *tmpMsg = (PSActionsPlugInMessage *)message;
+            BOOL status = EnumWindows(getPSMainWindowCB, (LPARAM)NULL);
+            assert(status != 0);
+            MessageBoxA(globalPSMainWindowHwnd, "Hello World!", "Tutorial Dialog", MB_OK|MB_ICONINFORMATION);
 		}
 	}
 
